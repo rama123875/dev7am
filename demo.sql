63a8de0dc38bb43994f325e80e0d7a0976091a92
root <root@ansible-mstr.us-central1-a.c.peaceful-guide-299405.internal>
2021-01-14 09:39:52 +0000

    Modified demo.sql

--- demo.sql
+++ demo.sql
@@ -1,6 +1,7 @@
 query to find list of managers
 
 select * from emp where empno in (select mgr from emp)
+select * from emp where empno in (select mgr from emp)
 
 
 select * from emp where sal in (select max(sal) from emp)
